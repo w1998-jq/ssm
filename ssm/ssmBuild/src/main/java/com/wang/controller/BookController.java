00c14de4ee7f05a8777ac34f2737be48fce76bb0
wang <2509434788@qq.com>
2021-11-23 15:42:59 +0800

    ssm

---
+++ ssm/ssmBuild/src/main/java/com/wang/controller/BookController.java
@@ -0,0 +1,59 @@
+package com.wang.controller;
+
+import com.wang.pojo.Books;
+import com.wang.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * @ClassName BookController
+ * Description
+ * @Author jqWang
+ * Date 2021/11/23 11:34
+ **/
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model){
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list",list);
+        return "allBook";
+    }
+
+    @RequestMapping("/addBook")
+    public String addPaper(Books books){
+        bookService.addBook(books);
+        return "redirect:/book/allBook";
+    }
+
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook(Model model,int id){
+        Books books = bookService.queryBookById(id);
+        model.addAttribute("book",books);
+        return "updateBook";
+    }
+    @RequestMapping("updateBook")
+    public String uodateBook(Model model,Books book){
+        bookService.updateBook(book);
+        Books books = bookService.queryBookById(book.getBookID());
+        model.addAttribute("books",books);
+        return "redirect:/book/allBook";
+    }
+
+    @RequestMapping("/del/{bookId}")
+    public String deleteBook(@PathVariable("bookId") int id){
+        bookService.deleteBookById(id);
+        return "redirect:/book/allBook";
+    }
+}
